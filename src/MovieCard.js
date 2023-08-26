029cb4f0fadee9bd9917d994dab465a4d6c00c32
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-08-26 16:03:39 +0530

    Set state and binding this

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,30 +1,47 @@
-import {Component} from "react";
+import { Component } from "react";
 import avangers from "./avengers.jpg"
 
-class MovieCard extends Component{
+class MovieCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "The Avengers!",
+            plot: "Supernatural powers shown in the movie.",
+            price: 199,
+            rating: 8.9
+        }
+        // this.addStars = this.addStars.bind(this);
+    }
 
-    render(){
-        return(
+   
+
+    addStars = ()=>{
+        console.log("this ",this);
+    }
+
+    render() {
+        const { title, plot, price, rating } = this.state;
+        return (
             <div className="main">
                 <div className="movie-card">
                     <div className="left">
                         <img src={avangers} alt="Poster" />
                     </div>
                     <div className="right">
-                        <div className="title">The Avenger</div>
-                        <div className="plot">Supernatural powers shown in the movie.</div>
-                        <div className="price">Rs. 199</div>
+                        <div className="title">{title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">Rs. {price}</div>
 
                         <div className="footer">
-                            <div className="rating">8.9</div>
+                            <div className="rating">{rating}</div>
                             <div className="star-dis">
-                                    <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/10337/10337430.png" />
-                                    <img alt="star" src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"className="stars"/>  
-                                    <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/10337/10337471.png" />
-                                    <span>0</span>
+                                <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/10337/10337430.png" />
+                                <img alt="star" src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" className="stars" />
+                                <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/10337/10337471.png" onClick={this.addStars} />
+                                <span>0</span>
                             </div>
                             <div className="favourite-btn">Favourite</div>
-                            <div className="cart-btn">Cart</div>
+                            <div className="cart-btn">Add to Cart</div>
                         </div>
                     </div>
                 </div>
