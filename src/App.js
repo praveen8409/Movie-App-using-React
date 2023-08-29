e5a093b4ace122bf65435b0e950351a5354de209
Praveen <praveen.kumar.paytm2019@gmail.com>
2023-08-29 17:48:57 +0530

    Implement props with the help of function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ function App() {
    <>
    {/* <h1>Movie App</h1> */}
   
-   <Student name="Praveen" marks={"80%"}/>
-   <Student name="Google" marks={"70%"}/>
-   <Student name="Alexa" marks={"60%"}/>
+   <Student name="Praveen" marks={80}/>
+   <Student name="Google" marks={70}/>
+   <Student name="Alexa" marks={60}/>
+   <Student />
    </>
   );
+}
+
+Student.defaultProps = {
+  name : "Student",
+  marks : "N.A."
 }
 
 export default App;
--- src/Student.js
+++ src/Student.js
@@ -1,16 +1,28 @@
-import React from "react";
+// import React from "react";
 
-class Student extends React.Component{
-    render(){
-        console.log(this.props)
-        return(
-            <>
-            <h1>Hello {this.props.name}</h1>
-            <h5>You have scored {this.props.marks}</h5>
+// class Student extends React.Component{
+//     render(){
+//         console.log(this.props)
+//         return(
+//             <>
+//             <h1>Hello {this.props.name}</h1>
+//             <h5>You have scored {this.props.marks} % </h5>
+//             <hr/>
+//             </>
+//         )
+//     }
+// }
+
+function Student(props){
+    const {name,marks} = props;
+
+    return (
+        <>
+             <h1>Hello {name}</h1>
+            <h5>You have scored {marks} % </h5>
             <hr/>
             </>
-        )
-    }
+    )
 }
 
 export default Student;
